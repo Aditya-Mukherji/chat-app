6ddee3b5e657d27a2797d52bdb62b09672d44d39
Aditya-Mukherji <adityamukherji405@gmail.com>
2024-02-24 15:48:42 +0530

    Completed the SignUP EndPoint with encrption using bcrypt.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,9 +1,55 @@
-export const signup = (req,res)=>{
-    console.log("Sign-Up")
-};
-export const login= (req,res)=>{
-    console.log("Log-In")
-};
-export const logout=(req,res)=>{
-    console.log("Log-Out")
-};
+import User from "../models/user.model.js"
+import bcrypt from "bcryptjs"
+export const signup =async (req,res)=>{
+    
+    try{
+        const {fullName,username,password,confirmPassword,gender} =req.body;
+        if(password !== confirmPassword){
+            return res.status(400).json({
+                error: "Passwords don't match"
+            })
+        }
+        const user =await User.findOne({username});
+        
+        if(user){
+            return res.status(400).json({
+                error:"Username already exists"
+            })
+        }
+        //HASH PASSWORD LOGIC
+        const salt= await bcrypt.genSalt(10)
+        const hashedPassword= await bcrypt.hash(password,salt)
+        //avatar placeholder logic using liara API
+
+        const boyProfilePic= 'https://avatar.iran.liara.run/public/boy?username=${username}'
+        const girlProfilePic= 'https://avatar.iran.liara.run/public/girl?username=${username}'
+
+        const newUser=new User({
+            fullName:fullName,
+            username:username,
+            password:hashedPassword,
+            gender:gender,
+            profilePic: gender==="male" ? boyProfilePic:girlProfilePic
+        })
+        await newUser.save();
+        res.status(201).json({
+            _id: newUser._id,
+            fullName:newUser.fullName,
+            username: newUser.username,
+            profilePic: newUser.profilePic
+        })
+    }
+    catch (error){
+        console.log("Erorr in signup controller", error.message)
+        res.status(500).json({
+            error:"Internal Server Error"
+        })
+    }
+   
+}
+export const login= async (req,res)=>{
+    res.send("hello")
+}
+export const logout =async (req,res)=>{
+    res.send("hello from logout")
+}
