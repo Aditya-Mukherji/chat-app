b3d08b0ef2b1c8b012c621023eff4c73cda1fd51
Aditya-Mukherji <adityamukherji405@gmail.com>
2024-02-24 18:09:05 +0530

    reorganized server

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ import express from "express"
 import dotenv from "dotenv"
 import authRoutes from "./routes/auth.routes.js"
 import connectToMongoDB from "./db/connectToMongoDB.js";
-
 const app= express()
 const PORT= process.env.PORT||5000
 
