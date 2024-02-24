38db49be43ae504bd92047f76471297e791faf00
Aditya-Mukherji <adityamukherji405@gmail.com>
2024-02-25 02:42:53 +0530

    added cookie-parser middleware

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
 import authRoutes from "./routes/auth.routes.js"
+import messageRoutes from "./routes/message.routes.js"
 import connectToMongoDB from "./db/connectToMongoDB.js";
 const app= express()
 const PORT= process.env.PORT||5000
@@ -8,7 +10,9 @@ const PORT= process.env.PORT||5000
 dotenv.config();
 
 app.use(express.json()) //To Parse incoming requests with a JSON payload
+app.use(cookieParser())
 app.use("/api/auth",authRoutes)
+app.use("/api/messages",messageRoutes)
 
 app.listen(PORT,()=>
 {   
