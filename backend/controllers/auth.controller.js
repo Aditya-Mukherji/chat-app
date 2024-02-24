d6c3fa100b12f006ccb089921cd1c67de9ee5d88
Aditya-Mukherji <adityamukherji405@gmail.com>
2024-02-24 14:51:12 +0530

    Added distinct routes using express and defind the Schema using mongoose

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export const signup = (req,res)=>{
+    console.log("Sign-Up")
+};
+export const login= (req,res)=>{
+    console.log("Log-In")
+};
+export const logout=(req,res)=>{
+    console.log("Log-Out")
+};
