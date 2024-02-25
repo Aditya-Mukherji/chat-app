86051a954ab06408235375ef63f0b9e6ccd7e9d1
Aditya-Mukherji <adityamukherji405@gmail.com>
2024-02-25 09:42:05 +0530

    Added routing for the logged in User

--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,7 +1,8 @@
 import express from "express"
-import sendMessage from "../controllers/message.controller.js"
+import Constants from "../controllers/message.controller.js"
 import protectRoute from "../middleware/protectRoute.middleware.js"
 const router =express.Router();
 
-router.post("/send/:id",protectRoute,sendMessage)
+router.get(":/id",protectRoute,Constants.getMessages)
+router.post("/send/:id",protectRoute,Constants.sendMessage)
 export default router
