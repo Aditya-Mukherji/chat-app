f7317a0750b8e13ccd8ab5587be04215c56c410a
Aditya-Mukherji <adityamukherji405@gmail.com>
2024-02-29 03:42:05 +0530

    Implemented a sidebar with search, list of users and a logout button

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import Conversatios from './Conversations'
 import SearchInput from './SearchInput'
-
+import LogoutButton from './LogoutButton'
 const Sidebar = () => {
   return (
-    <div>
-        <SearchInput/>
+    <div className='border-r border-slate-500 p-4 slex flex-col'>
+        <SearchInput />
         <div className='divider px-3'></div>
-        {/* <Conversation /> */}
-        {/* <LogoutButton /> */}
+        <Conversatios />
+        <LogoutButton />
     </div>
   )
 }
