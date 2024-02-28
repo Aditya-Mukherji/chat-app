1144b171ad4118fcb5599083395747b9ef1172b4
Aditya-Mukherji <adityamukherji405@gmail.com>
2024-02-29 03:43:42 +0530

    Added some code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,19 @@ import './App.css'
 import Login from './pages/login/Login'
 import SignUp from './pages/signup/signUp'
 import Home from './pages/home/Home'
-
+import {Routes,Route} from 'react-router-dom'
 function App() {
 
 
   return (
     <div className='p-4 h-screen flex items-center justify-center '>
-      <SignUp />
-      <Login/>
-      <Home/>
+     <Routes>
+
+      <Route path ='/' element= {<Home />}/>
+      <Route path ='/login' element= {<Login />}/>
+      <Route path ='/signup' element= {<SignUp />}/>
+
+     </Routes>
     </div>
   )
 }
