f7d9031a23d64432922b873f88cb86cf99a77fb3
Aditya-Mukherji <adityamukherji405@gmail.com>
2024-03-02 14:58:31 +0530

    Additional bug fixes

--- frontend/src/Hooks/useLogin.js
+++ frontend/src/Hooks/useLogin.js
@@ -27,6 +27,7 @@ const useLogin = () => {
             setLoading(false);
         }
     }
+    return {loading,login}
 }
 
 function handleInputErrors({username, password}) {
@@ -34,17 +35,6 @@ function handleInputErrors({username, password}) {
 		toast.error("Please fill in all fields");
 		return false;
 	}
-
-	if (password !== confirmPassword) {
-		toast.error("Passwords do not match");
-		return false;
-	}
-
-	if (password.length < 6) {
-		toast.error("Password must be at least 6 characters");
-		return false;
-	}
-
 	return true;
 }
 
