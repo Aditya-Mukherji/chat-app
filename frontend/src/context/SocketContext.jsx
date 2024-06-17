76198f9a79a2a0d5380a44af3948db13cd94ede9
adityam0105 <adityamukherji405@gmail.com>
2024-06-17 23:31:03 +0530

    Fixed socket url

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-9f0c.onrender.com/login", {
+			const socket = io("https://chat-app-9f0c.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
